6461fefd5379a78d1fd5b219237e5d9c3b07315a
wangtao <tao.wang@tenclass.com>
2021-03-10 11:41:13 +0800

    feat: 增加clean-webpack-plugin插件

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,18 @@
 const path = require('path')
 const htmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 module.exports = {
     mode: 'development',   // 指定构建模式
     entry: {
       'share/js': './src/index.js'
     },  // 指定构建入口文件
     output: {
-        // publicPath: 'https://www.lzwkcdn.com/', // 根据不用环境配置
+        // publicPath: 'https://www.lzwkcdn.com', // 根据不用环境配置
         filename: '[name].[chunkhash].js',  // 指定构建生成的文件名
         path: path.resolve(__dirname, 'dist/static'), // 指定构建生成文件所在路径
     },
     plugins: [
+      new CleanWebpackPlugin(),
       new htmlWebpackPlugin({
         inject: false,
         // excludeChunks: ['share'], // 排除后htmlWebpackPlugin.files也无法获取到
